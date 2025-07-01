e87f1ee40a566dc0b80da9938b6d654a6e71c582
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-07-01 20:04:56 +0900

    Update 팰린드롬인지 확인하기.cc

--- C++17/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ C++17/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
-int main() {
-	string iStr;
-	int strLength;
-	bool bFlag;
 
-	getline(cin, iStr);
-	strLength = iStr.length();
-	bFlag = true;
-	for (int i = 0; i < (strLength / 2); i++) {
-		if (iStr[i] != iStr[strLength - (i + 1)]) {
-			bFlag = false;
+string iStr, reverseStr;
+bool isPalindrome = true;
+int main() {
+	cin >> iStr;
+	reverseStr = iStr;
+	reverse(reverseStr.begin(), reverseStr.end());
+
+	for (int i = 0; i < iStr.length(); i++) {
+		if (iStr[i] != reverseStr[i]) {
+			isPalindrome = false;
 		}
 	}
-	if (bFlag) {
-		cout << 1;
+
+	if (isPalindrome) {
+		cout << '1';
 	}
 	else {
-		cout << 0;
+		cout << '0';
 	}
 }
