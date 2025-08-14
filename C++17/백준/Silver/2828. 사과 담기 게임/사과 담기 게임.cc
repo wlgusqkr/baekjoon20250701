5c52e85de4073abf169abafc3fd2a5612074308e
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-08-14 14:55:58 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N, M, J, result;
+int appleLocation, chai;
+
+int main(int argc, const char * argv[]) {
+    cin >> N >> M >> J;
+    int startNumber = 1, endNumber = M;
+    
+    for(int i = 0 ; i < J; i++) {
+        cin >> appleLocation;
+        
+        if(appleLocation >= startNumber && appleLocation <= endNumber) {
+            continue;
+        } else if (appleLocation > endNumber) {
+            chai = appleLocation - endNumber;
+            startNumber += chai; endNumber += chai;
+        } else if (appleLocation < startNumber) {
+            chai = startNumber - appleLocation;
+            startNumber -= chai; endNumber -= chai;
+        }
+        result += chai;
+    }
+    cout << result << '\n';
+}
