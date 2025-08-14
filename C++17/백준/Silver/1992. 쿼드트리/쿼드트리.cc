dc0dd5208672322e7e6253b94d4881182fe02ace
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-08-14 11:37:49 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- C++17/백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ C++17/백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,79 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void sub(int y1, int x1, int y2, int x2, string& str);
 int arr[70][70];
+
 string check(int y1, int x1, int y2, int x2) {
     int halfN = (y2 - y1) / 2;
     string str;
+    
+    sub(y1, x1, y1 + halfN, x1 + halfN, str);
+    sub(y1, x1 + halfN, y1 + halfN, x2, str);
+    sub(y1 + halfN, x1, y2, x1 + halfN, str);
+    sub(y1 + halfN, x1 + halfN, y2, x2, str);
+    
+    return "(" + str + ")";
+    
+}
+
+void sub(int y1, int x1, int y2, int x2, string& str) {
     bool hasZero, hasOne;
     
     hasZero = false;
     hasOne = false;
-    for(int i = y1 ; i < y1 + halfN; i++) {
-        for(int j = x1 ; j < x1 + halfN; j++) {
+    for(int i = y1 ; i < y2; i++) {
+        for(int j = x1 ; j < x2; j++) {
             if(arr[i][j] == 1) hasOne = true;
             if(arr[i][j] == 0) hasZero = true;
         }
     }
     if(hasZero == true && hasOne == true) {
-        str += check(y1, x1, y1 + halfN, x1 + halfN);
+        str += check(y1, x1, y2, x2);
     } else if (hasZero == true) {
         str += "0";
     } else if (hasOne == true) {
         str += "1";
     }
-    
-    hasZero = false;
-    hasOne = false;
-    for(int i = y1 ; i < y1 + halfN; i++) {
-        for(int j = x1 + halfN ; j < x2; j++) {
-            if(arr[i][j] == 1) hasOne = true;
-            if(arr[i][j] == 0) hasZero = true;
-        }
-    }
-    if(hasZero == true && hasOne == true) {
-        str += check(y1, x1 + halfN, y1 + halfN, x2);
-    } else if (hasZero == true) {
-        str += "0";
-    } else if (hasOne == true) {
-        str += "1";
-    }
-    
-    hasZero = false;
-    hasOne = false;
-    for(int i = y1 + halfN ; i < y2; i++) {
-        for(int j = x1 ; j < x1 + halfN; j++) {
-            if(arr[i][j] == 1) hasOne = true;
-            if(arr[i][j] == 0) hasZero = true;
-        }
-    }
-    if(hasZero == true && hasOne == true) {
-        str += check(y1 + halfN, x1, y2, x1 + halfN);
-    } else if (hasZero == true) {
-        str += "0";
-    } else if (hasOne == true) {
-        str += "1";
-    }
-    
-    hasZero = false;
-    hasOne = false;
-    for(int i = y1 + halfN ; i < y2; i++) {
-        for(int j = x1 + halfN ; j < x2; j++) {
-            if(arr[i][j] == 1) hasOne = true;
-            if(arr[i][j] == 0) hasZero = true;
-        }
-    }
-    if(hasZero == true && hasOne == true) {
-        str += check(y1 + halfN, x1 + halfN, y2, x2);
-    } else if (hasZero == true) {
-        str += "0";
-    } else if (hasOne == true) {
-        str += "1";
-    }
-    
-    
-    return "(" + str + ")";
-    
 }
 
 int main(int argc, const char * argv[]) {
