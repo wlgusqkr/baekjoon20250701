df055caf22c3d3dd8eb1d68269fa7f3ebcb9df8a
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-01 12:21:04 +0900

    [Gold IV] Title: 알파벳, Time: 1300 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[4] = { -1, 0, +1, 0 };
+int dx[4] = { 0, +1, 0, -1 };
+int R, C, visited[24][24], cnt;
+
+string visitedChar = "";
+char arr[24][24];
+
+void DFS(int y, int x) {
+    
+    
+    for(int i = 0 ; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if(ny >= R || ny < 0 || nx >= C || nx < 0 || visited[ny][nx]) continue;
+        if(visitedChar.find(arr[ny][nx]) != string::npos) continue;
+        visited[ny][nx] = visited[y][x] + 1;
+        visitedChar.push_back(arr[ny][nx]);
+        DFS(ny, nx);
+    }
+    cnt = max(visited[y][x], cnt);
+    visited[y][x] = 0;
+    auto iter = visitedChar.find(arr[y][x]);
+    visitedChar.erase(iter);
+}
+int main() {
+    cin >> R >> C;
+    
+    for(int i = 0; i < R; i++) {
+        for(int j = 0 ; j < C; j++) {
+            scanf(" %c", &arr[i][j]);
+        }
+    }
+    visitedChar.push_back(arr[0][0]);
+    DFS(0, 0);
+    cout << cnt + 1<< '\n';
+    
+}
