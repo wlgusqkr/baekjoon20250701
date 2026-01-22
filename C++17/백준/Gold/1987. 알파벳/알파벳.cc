51bdb507ea2c8b4911688219aa6ba357c71200ca
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-22 21:27:01 +0900

    [Gold IV] Title: 알파벳, Time: 432 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Gold/1987. 알파벳/알파벳.cc
+++ C++17/백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int R, C, visited[22][22], ret;
+
 int dy[4] = { -1, 0, +1, 0 };
 int dx[4] = { 0, +1, 0, -1 };
-int R, C, visited[24][24], cnt;
 
-string visitedChar = "";
-char arr[24][24];
+char arr[22][22];
 
-void DFS(int y, int x) {
+void go(int y, int x, int num, int cnt) {
+
+    ret = max(ret, cnt);
     for(int i = 0 ; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-
-        if(ny >= R || ny < 0 || nx >= C || nx < 0 || visited[ny][nx]) continue;
-        if(visitedChar.find(arr[ny][nx]) != string::npos) continue;
         
-        visited[ny][nx] = visited[y][x] + 1;
-        visitedChar.push_back(arr[ny][nx]);
-        
-        DFS(ny, nx);
-        
-        visited[ny][nx] = 0;
-        auto iter = visitedChar.find(arr[ny][nx]);
-        visitedChar.erase(iter);
+        if(ny >= R || ny < 0 || nx >= C || nx < 0 || visited[ny][nx] == 1) continue;
+        int _next = (1 << (int)(arr[ny][nx] - 'A'));
+        if((num & _next) == 0) go(ny, nx, num | _next, cnt + 1);
     }
-    cnt = max(visited[y][x], cnt);
+    return;
 }
 
 int main() {
+    
     cin >> R >> C;
     
-    for(int i = 0; i < R; i++) {
-        for(int j = 0 ; j < C; j++) {
+    for(int i = 0 ; i < R; i++) {
+        for(int j = 0; j < C; j++) {
             scanf(" %c", &arr[i][j]);
         }
     }
-    visitedChar.push_back(arr[0][0]);
-    DFS(0, 0);
-    cout << cnt + 1<< '\n';
+    
+    go(0, 0, 1 << (((int)arr[0][0]) - 'A'), 1);
+    
+    cout << ret;
     
 }
