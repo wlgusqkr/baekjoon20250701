8e5635b24d6b75ff54da04a554b8ccfe0b031900
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-02-07 12:48:09 +0900

    [Gold V] Title: AC, Time: 44 ms, Memory: 3212 KB -BaekjoonHub

--- C++17/백준/Gold/5430. AC/AC.cc
+++ C++17/백준/Gold/5430. AC/AC.cc
@@ -1,43 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-int T, N, x;
-string P, order;
-int main(){ 
-	cin >> T;  
-    for(int t = 0; t < T; t++){ 
-        deque<int> D;
-        cin >> P >> N >> order;
-        x = 0;
-        for(char c : order){
-        	if(c == '[' || c == ']') continue;
-            if(c >= '0' && c <= '9') x = x*10 + c-'0';
-            else{
-                if(x > 0) D.push_back(x);
-                x = 0; 
-            }
-		} 
-		if(x > 0) D.push_back(x);
-        bool error = false, rev = false;
- 		for(char a : P){
- 			if(a == 'R') rev = !rev; 
- 			else{
-                if(D.empty()){
-                    error = true;
-                    break;
+
+int T, n;
+char arr[100005];
+string str, arrStr;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> T;
+    for(int i = 0 ; i < T; i++) {
+        cin >> str;
+        cin >> n;
+        cin >> arrStr;
+        
+        deque<int> dq;
+        string numStr = "";
+        for (char c : arrStr) {
+            if (isdigit(c)) {
+                numStr += c;
+            } else {
+                if (!numStr.empty()) {
+                    dq.push_back(stoi(numStr));
+                    numStr = "";
                 }
-                if(rev) D.pop_back();
-                else D.pop_front(); 
-			}
-		}  
-        if(error) cout << "error" << '\n';
-        else{
-        	cout << "["; 
-            if(rev) reverse(D.begin(), D.end());
-            for(int i = 0; i < D.size(); i++){
-        		cout << D[i];  
-                if(i < D.size()-1) cout << ",";
             }
-        	cout << "]\n";  
         }
+        bool isReversed = false, isError = false;
+        for(char c : str) {
+            if(c == 'R') {
+                isReversed = !isReversed;
+            } else {
+                if(dq.empty()) { isError = true; break;}
+                if(isReversed) {
+                    dq.pop_back();
+                } else {
+                    dq.pop_front();
+                }
+            }
+        }
+        if(isError) {
+            cout << "error";
+        } else {
+            cout << "[";
+            if(isReversed) {
+                reverse(dq.begin(), dq.end());
+            }
+            for(int j = 0; j < dq.size(); j++) {
+                if(j == dq.size() - 1) cout << dq[j];
+                else cout << dq[j] << ",";
+            }
+            cout << "]";
+        }
+        cout <<'\n';
     }
-} 
+    
+}
