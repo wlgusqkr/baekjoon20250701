15d452cb9ef8e5e540fa4374663a3521edb8c651
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-02-13 16:44:37 +0900

    [Gold V] Title: 회의실 배정, Time: 80 ms, Memory: 3572 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, from, to, ret;
+vector<pair<int, int>> vec;
+
+int main() {
+    cin >> N;
+    for(int i = 0 ; i < N; i++) {
+        int start = 0, end = 0;
+        cin >> start >> end;
+        vec.push_back({end, start});
+    }
+    sort(vec.begin(), vec.end());
+    
+    to = vec[0].first; from = vec[0].second; ret++;
+    for(int i = 1 ; i < N; i++) {
+        if(vec[i].second < to || vec[i].first < to ) continue;
+        to = vec[i].first;
+        ret++;
+    }
+    cout << ret;
+    
+    
+}
